dbbb7cbfabd4c17b7dbd298af0bbc3dc914203fb
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2024-09-16 12:59:40 +0600

    done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
 import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
 
     const [menu , setMenu] = useState("home")
-    const {getTotalCartAmount} = useContext(StoreContext)
+    const {getTotalCartAmount, token, setToken} = useContext(StoreContext)
+    const navigate = useNavigate()
+    const logOut = () => {
+      localStorage.removeItem("token")
+      setToken("")
+      navigate("/")
+
+    }
   return (
     <div className='navbar'>
       <Link to="/"><img src={assets.logo} alt="" className='logo' /></Link>
       <ul className="navbar-menu">
-        <a href='#' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</a>
+        <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
         <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
         <a href='#app-download' onClick={() => setMenu("mobile app")} className={menu === "mobile app" ? "active" : ""}>Mobile app</a>
         <a href='#footer' onClick={() => setMenu("contact us")} className={menu === "contact us" ? "active" : ""}>Contact Us</a>
@@ -23,7 +30,22 @@ const Navbar = ({setShowLogin}) => {
         <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
             <div className={getTotalCartAmount() === 0 ? '' : "dot"}></div>
         </div>
-        <button onClick={() => setShowLogin(true)}>Sign in</button>
+        
+        {!token ? <button onClick={() => setShowLogin(true)}>Sign in</button> 
+        : <div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className="nav-profile-dropdown">
+            <li>
+              <img src={assets.bag_icon} alt="" />
+              <p>Orders</p>
+              </li>
+            <hr />
+            <li onClick={logOut}>
+              <img src={assets.logout_icon} alt="" />
+              <p>Logout</p>
+              </li>
+          </ul>
+          </div>}
       </div>
     </div>
   )
